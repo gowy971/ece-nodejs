875b6d6c966dc8b7144fd003f379dad4c4d8530a
gcoquerelle <gcoquerelle@gmail.com>
2015-11-25 20:21:43 +0100

    tests unitaires fonction user

--- test/test.js
+++ test/test.js
@@ -1,1 +1,22 @@
 var should = require('should');
+var user = require('../lib/users');
+
+describe("test save and function in user", function(){
+  it("Saves the user", function(){
+    user.save('Grégory',function(user){
+      user = {
+        name : user
+      }
+      user.should.have.property('name', 'Grégory');
+    });
+  });
+  it("Get the user by id", function(){
+    user.get(11,function(user){
+      var response = {
+        info : "C'est le user",
+        user : user
+      }
+      user.should.have.property('id', 11);
+  });
+});
+});
